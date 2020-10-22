f08b89c183037c749200d6324cac5e77ed69863a
Helen <melnikelena6969@gmail.com>
2020-10-22 19:40:25 +0300

    Updates

---
+++ precache-manifest.4506b77a6bc4a1e610d72c350ece0b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d5f81f97600574b737cf68e0dd4a25",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "fe9b156baa22c7f2dc92",
+    "url": "/Counter/static/css/main.acafc381.chunk.css"
+  },
+  {
+    "revision": "a8cc6e649bf015550918",
+    "url": "/Counter/static/js/2.2f8677e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.2f8677e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9b156baa22c7f2dc92",
+    "url": "/Counter/static/js/main.f07219a4.chunk.js"
+  },
+  {
+    "revision": "acc9df7a62210d2be914",
+    "url": "/Counter/static/js/runtime-main.27a9aae1.js"
+  }
+]);
